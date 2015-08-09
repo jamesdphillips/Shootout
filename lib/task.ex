303215f45b9b568621b34e94b6c97021a0604d70
James Phillips <jamesdphillips@gmail.com>
2015-08-09 10:52:43 -0700

    Run in parallel

--- lib/task.ex
+++ lib/task.ex
@@ -2,10 +2,24 @@ defmodule Mix.Tasks.Shootout.Run do
   use Mix.Task
 
   def run(_) do
-    IO.puts "=== Jiffy ==="
-    Shootout.jiffy
+    [
+      Task.async(&mochi/0),
+      Task.async(&poison/0),
+      Task.async(&jiffy/0)
+    ] |> Enum.map(&Task.await(&1, 60_000))
+  end
+
+  def mochi do
     IO.puts "=== Mochi ==="
     Shootout.mochi
+  end
+
+  defp jiffy do
+    IO.puts "=== Jiffy ==="
+    Shootout.jiffy
+  end
+
+  defp poison do
     IO.puts "=== Poison ==="
     Shootout.poison
   end
